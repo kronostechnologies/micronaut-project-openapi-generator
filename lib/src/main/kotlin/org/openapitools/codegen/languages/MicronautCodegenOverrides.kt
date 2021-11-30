994221a90f7641b655a5219eeed9af66f5ca2ef7
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2021-11-30 09:04:34 -0500

    Handle oneOf nullable construct

--- lib/src/main/kotlin/org/openapitools/codegen/languages/MicronautCodegenOverrides.kt
+++ lib/src/main/kotlin/org/openapitools/codegen/languages/MicronautCodegenOverrides.kt
@@ -1,5 +1,11 @@
 package org.openapitools.codegen.languages
 
+import io.micronaut.core.util.CollectionUtils
+import io.swagger.v3.oas.models.media.ComposedSchema
+import io.swagger.v3.oas.models.media.Schema
+import io.swagger.v3.oas.models.media.StringSchema
+import org.openapitools.codegen.utils.ModelUtils
+import org.slf4j.LoggerFactory
 import java.io.File
 
 data class OutputFolders(
@@ -13,6 +19,7 @@ open class MicronautCodegenOverrides(
 ) : MicronautCodegen() {
     private val generatedSourceFolder: String = "generated-sources" + File.separator + "openapi"
     private val generatedTestSourceFolder: String = "generated-test-sources" + File.separator + "openapi"
+    private val LOGGER = LoggerFactory.getLogger(AbstractJavaCodegen::class.java)
 
     override fun processOpts() {
         super.processOpts()
@@ -34,5 +41,34 @@ open class MicronautCodegenOverrides(
             else -> folder
         }
         super.addSupportingFile(projectFolder, packageString, file)
+    }
+
+    override fun getSchemaType(schema: Schema<Any>): String? {
+        val format = schema.format
+        if (schema is StringSchema && format != null && CUSTOM_FORMATS.containsKey(format)) {
+            val type = CUSTOM_FORMATS.get(format)!!.name
+            LOGGER.warn("Use custom format {} with type {}.", format, type)
+            return type
+        }
+        if (schema is ComposedSchema) {
+            if (CollectionUtils.isNotEmpty(schema.oneOf as Collection<*>?)) {
+                if(schema.nullable && schema.oneOf.count() == 1) {
+                    val inner: Schema<Any>? = schema.oneOf
+                        .stream()
+                        .filter {
+                                subSchema -> !ModelUtils.isNullType(subSchema)
+                        }
+                        .findFirst()
+                        .orElse(null)
+                    if(inner != null) {
+                        return super.getSchemaType(inner)
+                    }
+                }
+
+                // ignore embedded oneOf schemas
+                return Any::class.java.name
+            }
+        }
+        return super.getSchemaType(schema)
     }
 }
